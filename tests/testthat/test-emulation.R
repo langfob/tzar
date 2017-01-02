492554dc11496fa8083d60805ec08786de4be107
Bill Langford <bill.langford@rmit.edu.au>
2017-01-02 14:19:50 +1100

    Create primary test structure with dummy tests that always succeed for the moment.

---
+++ tests/testthat/test-emulation.R
@@ -0,0 +1,14 @@
+context("calling mainline with and without tzar and/or emulation")
+
+test_that("tzar emulation works", {
+expect_equal(TRUE,TRUE)
+})
+
+test_that("tzar works without emulation", {
+expect_equal(TRUE,TRUE)
+})
+
+test_that("not using tzar at all works", {
+expect_equal(TRUE,TRUE)
+})
+
--- tests/testthat/test-placeholder.R
+++
@@ -1,6 +0,0 @@
-context("placeholder test until you create real tests")
-
-test_that("spurious test of equality to make CHECK happy", {
-expect_equal(TRUE,TRUE)
-})
-
