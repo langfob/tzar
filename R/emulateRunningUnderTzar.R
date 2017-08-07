f4f55aa2d5a83174594ff4a1865ad0cbae0fb326
Bill Langford <bill.langford@rmit.edu.au>
2017-08-07 14:37:00 +1000

    Remove camel case

--- R/emulateRunningUnderTzar.R
+++ R/emulateRunningUnderTzar.R
@@ -5,17 +5,17 @@
 #===============================================================================
 
 emulate_running_tzar = function (project_path,
-                                 tzarEmulation_scratchFileName
+                                 tzar_emulation_scratch_file_name
                                  )
     {
         #-----------------------------------------------------------------------
         #  Probably never need to change these...
         #-----------------------------------------------------------------------
 
-    tzarParametersSrcFileName           = "parameters.R"
-    tzarEmulation_completedDirExtension = ".completedTzarEmulation"
-    tzarInProgressExtension             = ".inprogress/"
-    tzarFinishedExtension               = ""
+    tzar_parameters_src_file_name          = "parameters.R"
+    tzar_emulation_completed_dir_extension = ".completedTzarEmulation"
+    tzar_in_progress_extension             = ".inprogress/"
+    tzar_finished_extension                = ""
 
         #-----------------------------------------------------------------------
         #  Need to set these variables just once, i.e., at start of a new project.
@@ -36,8 +36,8 @@ emulate_running_tzar = function (project_path,
     # #tzar_jar_path = "~/D/rdv-framework/tzar.jar"
     # tzar_jar_path = "~/D/rdv-framework-latest-work/tzar.jar"
 
-    # #tzarEmulation_scratchFileName = "~/D/rdv-framework/projects/rdvPackages/biodivprobgen/R/tzarEmulation_scratchFile.txt"
-    # tzarEmulation_scratchFileName = "~/D/Projects/ProblemDifficulty/src/bdprobdiff/R/tzarEmulation_scratchFile.txt"
+    # #tzar_emulation_scratch_file_name = "~/D/rdv-framework/projects/rdvPackages/biodivprobgen/R/tzarEmulation_scratchFile.txt"
+    # tzar_emulation_scratch_file_name = "~/D/Projects/ProblemDifficulty/src/bdprobdiff/R/tzarEmulation_scratchFile.txt"
 
         #-----------------------------------------------------------------------
         #  Basing all of the stuff below on reading the rrunner.R file.
@@ -87,18 +87,18 @@ emulate_running_tzar = function (project_path,
             #      tzarEmulation_scratchFile.txt
             #  Resulting dir name is something like:
             #      "/Users/bill/tzar/outputdata/biodivprobgen/default_runset/827_default_scenario.inprogress/"
-#    tzarInProgressDirName = readLines (tzarEmulation_scratchFileName)
-    tzarInProgressDirName =
-            get_tzarOutputDir_from_scratch_file (tzarEmulation_scratchFileName)
+#    tzar_in_progress_dir_name = readLines (tzar_emulation_scratch_file_name)
+    tzar_in_progress_dir_name =
+            get_tzarOutputDir_from_scratch_file (tzar_emulation_scratch_file_name)
 
             #  Build the name of the directory that would result if tzar
             #  successfully ran to completion without emulation, e.g.,
             #      /Users/bill/tzar/outputdata/biodivprobgen/default_runset/828_default_scenario
 
-    tzarFinishedDirName =
-        gsub (tzarInProgressExtension,  #  e.g., ".inprogress/"
-              tzarFinishedExtension,    #  e.g., ""
-              tzarInProgressDirName)    #  The name loaded in previous command
+    tzar_finished_dir_name =
+        gsub (tzar_in_progress_extension,  #  e.g., ".inprogress/"
+              tzar_finished_extension,    #  e.g., ""
+              tzar_in_progress_dir_name)    #  The name loaded in previous command
 
             #  That is the name that will have been hung on the results of
             #  using the dummy model.R code to get tzar to build the
@@ -109,16 +109,16 @@ emulate_running_tzar = function (project_path,
             #  during the dummy model.R run, we need to change the finished
             #  directory's name back to the .inprogress extension.
 browser()
-    file.rename (tzarFinishedDirName, tzarInProgressDirName)
+    file.rename (tzar_finished_dir_name, tzar_in_progress_dir_name)
 
         #-----------------------------------------------------------
         #  Finally, load the parameters list that tzar created and
         #  saved as an R file.
         #----------------------------------------------------------
 
-    parametersListSourceFilename = paste0 (tzarInProgressDirName,
+    parametersListSourceFilename = paste0 (tzar_in_progress_dir_name,
                                            "metadata/",  #  BTL added 2014 12 28
-                                           tzarParametersSrcFileName)
+                                           tzar_parameters_src_file_name)
     source (parametersListSourceFilename)
 
         #-----------------------------------------------------------
@@ -127,12 +127,12 @@ browser()
         #  finishes, if it finishes successfully.
         #----------------------------------------------------------
 
-    parameters$tzarInProgressDirName = tzarInProgressDirName
+    parameters$tzar_in_progress_dir_name = tzar_in_progress_dir_name
 
     parameters$tzarEmulationCompletedDirName =
-        paste0 (tzarFinishedDirName, tzarEmulation_completedDirExtension)
+        paste0 (tzar_finished_dir_name, tzar_emulation_completed_dir_extension)
 
-    parameters$tzarEmulation_scratchFileName = tzarEmulation_scratchFileName
+    parameters$tzar_emulation_scratch_file_name = tzar_emulation_scratch_file_name
 
     return (invisible (parameters))
     }
