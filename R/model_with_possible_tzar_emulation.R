c7b6564e60d8003b8bc78c7959d02834ea0da7c1
Bill Langford <bill.langford@rmit.edu.au>
2017-08-07 17:31:50 +1000

    Use values from yaml file instead of arg list

--- R/model_with_possible_tzar_emulation.R
+++ R/model_with_possible_tzar_emulation.R
@@ -10,28 +10,16 @@
 #'
 #' @param parameters  list of parameters built by tzar from project.yaml
 #' @param main_function  name of function to call to run under tzar or tzar emulation
-#' @param projectPath path of R code and project.yaml file for project
-#' @param emulation_scratch_file_path path of scratch file for passing tzarEmulation flag and tzarOutputDir between tzar and mainline function
 #'
 #' @return parameters list of parameters loaded from project.yaml file
 #' @export
 #'
 #' @examples \dontrun{
-#' model_with_possible_tzar_emulation (parameters,
-#'                                     main_function                 = trial_main_function,
-#'                                     projectPath                   = ".",
-#'                                     emulation_scratch_file_path   = "~/tzar_emulation_scratch.yaml"
-#'                                    )
+#' model_with_possible_tzar_emulation (parameters, my_main_function)
 #'}
 
-model_with_possible_tzar_emulation <- function (parameters,
-                                                main_function,
-                                                projectPath,
-                                                emulation_scratch_file_path
-                                                )
+model_with_possible_tzar_emulation <- function (parameters, main_function)
     {
-    # emulatingTzar =
-    #     get_emulating_tzar_from_scratch_file (emulation_scratch_file_path)
     emulating_tzar = as_boolean (parameters$emulatingTzar)
 
     if (! emulating_tzar)
@@ -40,14 +28,14 @@ model_with_possible_tzar_emulation <- function (parameters,
 
         main_function (parameters = parameters)
 
-        } else                     #  EMULATING
+        } else
+        #---------------------  EMULATING  ---------------------
         {
         cat ("\n\n=====>  In model.R: EMULATING tzar")
 
-        set_tzarOutputDir_in_scratch_file (parameters$fullOutputDirWithSlash,
-                                           emulation_scratch_file_path)
 
-        parameters$tzarEmulation_scratchFileName = emulation_scratch_file_path
+        set_tzarOutputDir_in_scratch_file (parameters$fullOutputDirWithSlash,
+                                           parameters$emulation_scratch_file_path)
         }
 
     return (parameters)
