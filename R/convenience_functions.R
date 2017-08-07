866b90fb787c227bc78cf9129b2a4b5e212ed751
Bill Langford <bill.langford@rmit.edu.au>
2017-08-07 10:25:54 +1000

    Add convenience functions moved from xubdpgpaper

--- R/convenience_functions.R
+++ R/convenience_functions.R
@@ -48,3 +48,141 @@ run_tzar_outside_pkg <-
     }
 
 #===============================================================================
+
+#' Convenience function to run tzar in pkg with all appropriate variables set
+#'
+#' The name stands for "run Tzar Inside Package".  The function is intended to
+#' be quick and easy to call over and over from the command line, so its
+#' name is short and the call has no arguments.
+#'
+#' @param parameters List of parameters controlling the current run (usually
+#'   decoded from project.yaml by tzar)
+#'
+#' @return Returns nothing
+#' @export
+#'
+#' @examples \dontrun{
+#' runtip ()
+#'}
+
+runtip <- function (parameters_yaml_file_path = "./project.yaml")
+    {
+    parameters = yaml::yaml.load_file (parameters_yaml_file_path)
+
+    emulating_tzar                     = TRUE
+    main_function                      = tzar_main
+    project_path                       = "/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpgxupaper/R"
+    emulation_scratch_file_path        = "~/tzar_emulation_scratch.yaml"
+    tzar_jar_path                      = "~/D/rdv-framework-latest-work/tzar.jar"
+
+    copy_model_R_tzar_file             = TRUE
+    model_R_tzar_src_dir               = "/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpgxupaper/R"    #"."    #system.file("templates", package="bdpgxupaper")
+    model_R_tzar_disguised_filename    = "model.R.tzar"
+    overwrite_existing_model_R_dest    = TRUE
+    required_model_R_filename_for_tzar = "model.R"
+
+    return (
+        run_tzar (
+              emulating_tzar                     = emulating_tzar,
+              main_function                      = main_function,
+              project_path                       = project_path,
+              emulation_scratch_file_path        = emulation_scratch_file_path,
+              tzar_jar_path                      = tzar_jar_path,
+
+              copy_model_R_tzar_file             = copy_model_R_tzar_file,
+              model_R_tzar_src_dir               = model_R_tzar_src_dir,
+              model_R_tzar_disguised_filename    = model_R_tzar_disguised_filename,
+              overwrite_existing_model_R_dest    = overwrite_existing_model_R_dest,
+              required_model_R_filename_for_tzar = required_model_R_filename_for_tzar
+              )
+            )
+    }
+
+#-------------------------------------------------------------------------------
+
+#' Convenience function to run tzar outside pkg with all appropriate variables set
+#'
+#' The name stands for "run Tzar Outside Package".  The function is intended to
+#' be quick and easy to call over and over from the command line, so its
+#' name is short and the call has no arguments.
+#'
+#' @param parameters List of parameters controlling the current run (usually
+#'   decoded from project.yaml by tzar)
+#'
+#' @return Returns nothing
+#' @export
+#'
+#' @examples \dontrun{
+#' runtop ()
+#'}
+
+runtop <- function (parameters_yaml_file_path = "./project.yaml")
+    {
+    emulating_tzar                     = TRUE
+    main_function                      = tzar_main
+    project_path                       = "/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpgxupaper/R"
+    emulation_scratch_file_path        = "~/tzar_emulation_scratch.yaml"
+    tzar_jar_path                      = "~/D/rdv-framework-latest-work/tzar.jar"
+
+    copy_model_R_tzar_file             = FALSE
+
+    model_R_tzar_src_dir               = NULL
+    model_R_tzar_disguised_filename    = NULL
+    overwrite_existing_model_R_dest    = NULL
+    required_model_R_filename_for_tzar = NULL
+
+    return (
+        run_tzar (
+              emulating_tzar                     = emulating_tzar,
+              main_function                      = main_function,
+              project_path                       = project_path,
+              emulation_scratch_file_path        = emulation_scratch_file_path,
+              tzar_jar_path                      = tzar_jar_path,
+
+              copy_model_R_tzar_file             = copy_model_R_tzar_file,
+              model_R_tzar_src_dir               = model_R_tzar_src_dir,
+              model_R_tzar_disguised_filename    = model_R_tzar_disguised_filename,
+              overwrite_existing_model_R_dest    = overwrite_existing_model_R_dest,
+              required_model_R_filename_for_tzar = required_model_R_filename_for_tzar
+              )
+            )
+    }
+
+#-------------------------------------------------------------------------------
+
+#' Convenience function to run tzar WITHOUT tzar emulation
+
+#' The name stands for "run Tzar Without Emulation".  The function is intended to
+#' be quick and easy to call over and over from the command line, so its
+#' name is short and the call has no arguments.
+#'
+#' Unlike runtip() and runtop(), this function is intended to be equivalent
+#' to running tzar from the command line WITHOUT emulation.
+#'
+#' @param parameters List of parameters controlling the current run (usually
+#'   decoded from project.yaml by tzar)
+#'
+#' @return Returns nothing
+#' @export
+#'
+#' @examples \dontrun{
+#' runtwoe ()
+#'}
+
+runtwoe <- function (parameters_yaml_file_path = "./project.yaml")
+    {
+    parameters = yaml::yaml.load_file (parameters_yaml_file_path)
+
+    # project_path                       = "/Users/bill/D/Projects/ProblemDifficulty/pkgs/bdpgxupaper/R"
+    # tzar_jar_path                      = "~/D/rdv-framework-latest-work/tzar.jar"
+
+    project_path                       = parameters$project_path
+    tzar_jar_path                      = parameters$tzar_jar_path
+
+    return (
+            tzar_jar_path                      = tzar_jar_path,
+            project_path                       = project_path
+              )
+    }
+
+#===============================================================================
